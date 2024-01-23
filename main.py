76bc616cab9874bb766c5c02df656bd5cac6a007
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2024-01-23 17:38:24 -0300

    feat: run with reload automatically if is development

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ app = FastAPI()
 assistant_id = getenv("ASSISTANT_ID")
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="0.0.0.0", port=3000)
+  isDevelopment = getenv("APP_ENV") == "development"
+
+  uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=isDevelopment)
 
   if assistant_id == None:
     assistant_id = createAssistant()
